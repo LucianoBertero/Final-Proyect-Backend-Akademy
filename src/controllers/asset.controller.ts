15639f6e660f72a52bb3cc4efb2940e64a3dc1bd
LucianoBertero <68975671+LucianoBertero@users.noreply.github.com>
2024-09-02 17:09:51 -0300

    refactor: Refactor AssetController registerAsset method
    - Refactor the registerAsset method in the AssetController class to improve code readability and maintainability.
    - Clean up unnecessary console.log statements and commented out code.
    - Validate and obtain the category before creating a new asset.
    - Validate and obtain the assigned employee (if provided) before creating a new asset.
    - Handle the assigned date (if provided) and validate the date format.
    - Create and save the new asset in the database.
    
    Fixes #123

--- src/controllers/asset.controller.ts
+++ src/controllers/asset.controller.ts
@@ -1,34 +1,26 @@
 import mongoose from "mongoose";
-import { Request } from "express";
-import { Response } from "express";
+import { Request, Response } from "express";
 
-import User from "../models/User";
 import Asset from "../models/Asset";
 import Category from "../models/Category";
 import Employee from "../models/Employee";
 
 import responseModel from "../helpers/response.model";
 
-import IUser from "../interface/user.interface";
-
 class AssetController {
   static async registerAsset(req: Request, res: Response) {
     const { name, description, category, assigned_employee, assigned_date } =
       req.body;
 
-    console.log("entro");
-    //TODO manejar con ids
-
     const session = await mongoose.startSession();
     session.startTransaction();
 
     try {
-      const existingAset = await Asset.findOne({ name }).session(session);
-
-      if (existingAset) {
+      // Verificar si el activo ya existe
+      const existingAsset = await Asset.findOne({ name }).session(session);
+      if (existingAsset) {
         await session.abortTransaction();
         session.endSession();
-
         return responseModel.fail(
           req,
           res,
@@ -36,6 +28,8 @@ class AssetController {
           422
         );
       }
+
+      // Validar y obtener la categoría
       if (!mongoose.Types.ObjectId.isValid(category)) {
         return responseModel.fail(
           req,
@@ -47,25 +41,9 @@ class AssetController {
       const existingCategory = await Category.findOne({
         _id: category,
       }).session(session);
-      console.log(
-        "🚀 ~ AssetController ~ registerAsset ~ existingCategory:",
-        existingCategory
-      );
-
-      console.log(existingCategory);
-
-      if (!mongoose.Types.ObjectId.isValid(assigned_employee)) {
-        return responseModel.fail(
-          req,
-          res,
-          { message: "Invalid employee ID" },
-          422
-        );
-      }
       if (!existingCategory) {
         await session.abortTransaction();
         session.endSession();
-
         return responseModel.fail(
           req,
           res,
@@ -74,41 +52,72 @@ class AssetController {
         );
       }
 
-      const existingEmployee = await Employee.findOne({
-        _id: assigned_employee,
-      }).session(session);
-
-      if (!existingEmployee) {
-        await session.abortTransaction();
-        session.endSession();
-
-        return responseModel.fail(
-          req,
-          res,
-          { message: "Employee not found" },
-          422
-        );
+      // Validar y obtener el empleado asignado (si se proporciona)
+      let employeeId = null;
+      if (assigned_employee) {
+        if (!mongoose.Types.ObjectId.isValid(assigned_employee)) {
+          return responseModel.fail(
+            req,
+            res,
+            { message: "Invalid employee ID" },
+            422
+          );
+        }
+        const existingEmployee = await Employee.findOne({
+          _id: assigned_employee,
+        }).session(session);
+        if (!existingEmployee) {
+          await session.abortTransaction();
+          session.endSession();
+          return responseModel.fail(
+            req,
+            res,
+            { message: "Employee not found" },
+            422
+          );
+        }
+        employeeId = existingEmployee._id;
       }
 
-      //fecha
+      // Manejo de la fecha asignada (si se proporciona)
+      let assignedDate = null;
+      if (assigned_date) {
+        assignedDate = new Date(assigned_date);
+        if (isNaN(assignedDate.getTime())) {
+          return responseModel.fail(
+            req,
+            res,
+            { message: "Invalid date format" },
+            422
+          );
+        }
+      }
 
+      // Crear y guardar el nuevo activo
       const newAsset = new Asset({
         name,
         description,
         category: existingCategory._id,
-        assigned_employee: existingEmployee._id,
-        assigned_date: assigned_date,
+        assigned_employee: employeeId,
+        assigned_date: assignedDate,
       });
 
       await newAsset.save({ session: session });
-
       await session.commitTransaction();
       session.endSession();
 
       return responseModel.success(req, res, { message: "Asset created" }, 201);
     } catch (error) {
-      console.log(error);
+      await session.abortTransaction();
+      session.endSession();
+      return responseModel.fail(
+        req,
+        res,
+        { message: "Error al registrar el activo" },
+        500
+      );
     }
   }
 }
+
 export default AssetController;
